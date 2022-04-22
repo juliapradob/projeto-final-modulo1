7963dd9f0b5158eb509ba66a6da8b8ea13a19fdb
Julia Prado <juliapbasile@gmail.com>
2022-04-22 14:18:49 -0300

    Ajusta links na escolha

--- script/escolhe.js
+++ script/escolhe.js
@@ -3,9 +3,9 @@ function agwen(){
 }
 
 function bylim(){
-    window.location = ("../personagens/bylim1.html");
+    window.location = ("./personagens/bylim1.html");
 }
 
 function hortan(){
-    window.location = ("../../personagens/hortan1.html");
+    window.location = ("./personagens/hortan1.html");
 }
